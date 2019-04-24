9cdaa22bfc65b26a233a4599d112d7f08b41bb26
Joaquin Peñalver <joaquin.penalver@gmail.com>
2019-04-25 00:49:48 +0200

    first commit

---
+++ src/test/java/micronaut/hello/world/controller/HelloControllerSpec.java
@@ -0,0 +1,46 @@
+package micronaut.hello.world.controller;
+
+import io.micronaut.context.ApplicationContext;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.runtime.server.EmbeddedServer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloControllerSpec {
+
+    private static EmbeddedServer server;
+    private static HttpClient client;
+
+    @BeforeAll
+    public static void setupServer() {
+
+        server = ApplicationContext.run(EmbeddedServer.class);
+
+        client = server
+                .getApplicationContext()
+                .createBean(HttpClient.class, server.getURL());
+    }
+
+    @AfterAll
+    public static void stopServer() {
+        if (client != null) {
+            client.stop();
+        }
+        if (server != null) {
+            server.stop();
+        }
+    }
+
+    @Test
+    public void testHelloWorldResponse() {
+
+        String response = client.toBlocking()
+                .retrieve(HttpRequest.GET("/hello"));
+
+        assertEquals("Hello World", response);
+    }
+}
